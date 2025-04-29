512bc158ac4d11cfb8b7e97da0359eb0534415d3
Austin Meredith <adm21c@acu.edu>
2025-04-28 21:00:22 -0500

    got cmake to work

---
+++ Build/CMakeFiles/GoalTracker.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GoalTracker.dir/src/app/goal.cpp.obj"
+  "CMakeFiles/GoalTracker.dir/src/app/goal.cpp.obj.d"
+  "CMakeFiles/GoalTracker.dir/src/app/progressLog.cpp.obj"
+  "CMakeFiles/GoalTracker.dir/src/app/progressLog.cpp.obj.d"
+  "CMakeFiles/GoalTracker.dir/src/main.cpp.obj"
+  "CMakeFiles/GoalTracker.dir/src/main.cpp.obj.d"
+  "GoalTracker.exe"
+  "GoalTracker.exe.manifest"
+  "GoalTracker.pdb"
+  "libGoalTracker.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GoalTracker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Build/CMakeFiles/GoalTracker.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for GoalTracker.
+# This may be replaced when dependencies are built.
