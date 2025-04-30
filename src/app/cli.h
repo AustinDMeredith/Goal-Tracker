b991476ce7ab8fbba0941d4b4edac22fbb83752d
Austin Meredith <109480201+AustinDMeredith@users.noreply.github.com>
2025-04-30 14:21:05 -0500

    thinking about all the functions i want to add to the cli

---
+++ src/app/cli.h
@@ -0,0 +1,17 @@
+#ifndef CLI_H
+#define CLI_H
+
+void printMenu();
+void printHelp();
+void printVersion();
+// function to handle the command line arguments
+// function to handle creating a goal
+// function to handle deleting a goal
+// function to handle logging progress
+// function to handle listing goals and progress
+// function to handle marking a goal as complete
+// function to handle setting a reminder
+// function to handle changing a deadline
+// function to handle changing a goal's priority
+
+#endif 
