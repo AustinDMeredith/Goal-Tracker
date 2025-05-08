0cfe575eb8522003c051a15b05341138426f51be
Austin Meredith <adm21c@acu.edu>
2025-05-07 21:30:36 -0500

    goalStorage.h done

--- src/core/goalStorage.h
+++ src/core/goalStorage.h
@@ -5,10 +5,18 @@
 #include <map>
 #include <string>
 
+class GoalStorage
+{
+private:
+    std::map<std::string, Goal> goals; // Map to store goals with their IDs as keys
 
-
-
-
+public:
+    void addGoal(const std::string& id, const Goal& goal); // Add a goal to the storage
+    void removeGoal(const std::string& id); // Remove a goal from the storage
+    void displayGoals() const; // Display all goals in the storage
+    Goal* findGoal(const std::string& id); // Find a goal by its ID
+    void updateGoal(const std::string& id, const Goal& updatedGoal); // Update a goal in the storage
+};
 
 #endif
 
